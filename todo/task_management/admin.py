170a7b4d344019e456c0ed681c6f5f2e0684e588
Ashish Bhardwaj <erashish41@gmail.com>
2026-01-07 00:35:59 +0530

    updatd admin, models and templates

--- todo/task_management/admin.py
+++ todo/task_management/admin.py
@@ -6,13 +6,27 @@ from task_management.models import (
 # Register your models here.
 
 admin.site.register(SubTask)
-admin.site.register(Category)
-admin.site.register(Tag)
 admin.site.register(Comment)
 admin.site.register(Attachment)
 
 @admin.register(Task)
-class ToDoAdmin(admin.ModelAdmin):
+class TaskAdmin(admin.ModelAdmin):
     list_display = ['title', 'priority', 'status', 'due_date']
     list_filter = ['priority', 'status', 'due_date']
     search_fields = ['title', 'priority']
+    
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_by')
+    search_fields = ('name',)
+    list_filter = ('created_by',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_by')
+    search_fields = ('name',)
+    list_filter = ('created_by',)
+
+
