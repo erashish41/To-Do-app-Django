450c6c41366034708a1fd1e2821dbede4486e6fb
Ashish Bhardwaj <erashish41@gmail.com>
2026-01-28 22:53:11 +0530

    task api with FBV crud

--- todo/task_management/api_modules/urls.py
+++ todo/task_management/api_modules/urls.py
@@ -1,8 +1,11 @@
 # urls.py
 
 from django.urls import path
-from task_management.api_modules.views import taskView
+from task_management.api_modules.views import (
+    taskView, taskDetail
+    )
 
 urlpatterns = [
-    path('v2/task/', taskView, name='task'),
+    path('v2/task/', taskView, name='task_list'),
+    path('v2/task/<uuid:pk>/', taskDetail, name='task_detail'),
 ]
--- todo/task_management/api_modules/views.py
+++ todo/task_management/api_modules/views.py
@@ -7,9 +7,38 @@ from task_management.models import (
 )
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def taskView(request):
     if request.method == "GET":
         task = Task.objects.all()
         serializer = TaskSerializer(task, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(["GET", "PUT", "DELETE"])
+def taskDetail(request, pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == "GET":
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == "PUT":
+        serializer = TaskSerializer(task,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == "DELETE":
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
